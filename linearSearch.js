2668845f156873dbf2c698987b640d3e42807ef1
and3k <andrewjkasten@gmail.com>
2025-11-10 13:58:39 -0700

    for loop solution

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,27 @@
 function linearSearch(searchTerm, arr) {
-  return undefined;
+ 
+ for (let i = 0; i < arr.length; i++){
+ 
+  if (searchTerm === arr[i]){
+     return i
+  }
+  
+ }
+ 
 }
+let bananaList = "bananas".split(""); // ["b", "a", "n", "a", "n", "a", "s"]
+
 
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  let search = []
+for (let i = 0; i < arr.length; i++){
+  if (searchTerm === arr[i]){
+    search.push(i)
+  }
 }
+return search
+}
+
+console.log(globalLinearSearch("a", bananaList)); // [ 1, 3, 5 ]
 
 module.exports = { linearSearch, globalLinearSearch };
